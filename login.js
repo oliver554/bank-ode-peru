f6dc6990ed65d4a635cdaa9a802eea5685605518
Celestine Akpanoko <52492708+Celestine4cpp@users.noreply.github.com>
2021-01-31 01:00:52 +0000

    Update login.js

--- login.js
+++ login.js
@@ -3,7 +3,7 @@ var username = document.getElementById("usr").value;
 var password = document.getElementById("pwd").value;
     if (username == "ryan2519" && password == "Wayne098123!?!"){
         alert("Login is successful!");
-        document.login_form.action = "Dashboard.html";
+        document.login_form.action = "dashboard.html";
         // alert(document.login_form.action);
         // window.location.replace("Bank Of Peru_Dashboard.html");
         return true;
